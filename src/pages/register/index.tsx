defb4a71bae42d123d7f0c06fa33451c321e04b9
ekim <eunkukkim1992@gmail.com>
2023-02-06 15:13:45 +0900

    [N]- register page

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -1,16 +1,19 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEyeSlash, faUser, faEye } from '@fortawesome/free-solid-svg-icons'
+import { faEyeSlash, faUser, faEye, faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import { ChangeEvent, useState } from 'react'
 import { httpClient } from '@/apis'
+import { schema, NavigationUtil } from '@/utils'
 import { useNavigate } from 'react-router'
-import { NavigationUtil } from '@/utils'
 
-const LoginPage = () => {
+const Register = () => {
   const navigate = useNavigate()
   const [isShow, setIsShow] = useState<boolean>(false)
+  const [isShowPass, setIsShowPass] = useState<boolean>(false)
   const [inputs, setInputs] = useState({
     id: '',
+    username: '',
     password: '',
+    passwordConfirm: '',
   })
 
   const onChangeInputs = (event: ChangeEvent<HTMLInputElement>): void => {
@@ -24,29 +27,37 @@ const LoginPage = () => {
     setIsShow((prev) => !prev)
   }
 
-  const onClickLogin = async () => {
-    const { id, password } = inputs
-    if (!id || !password) return alert('정보를 입력해 주세요')
-    try {
-      await httpClient.auth.login({ ...inputs })
-    } catch (error) {
-      if (error instanceof Error) console.log(error.message)
-    }
+  const onClickShowPassConfirm = (): void => {
+    setIsShowPass((prev) => !prev)
   }
 
-  const onClickMoveToRegister = () => {
-    navigate(NavigationUtil.register)
+  const onClickRegister = async () => {
+    schema({ ...inputs })
+
+    try {
+      await httpClient.users.createUser({ ...inputs })
+    } catch (error) {
+      if (error instanceof Error) console.log('register error:', error.message)
+    }
+
+    navigate(NavigationUtil.login)
   }
 
   return (
     <div className='w-screen h-screen flex items-center justify-center bg-bkg'>
       <div className='w-518 h-471 bg-white shadow-1 flex flex-col items-center justify-evenly rounded-lg'>
-        <h1 className='w-72 h-10 font-bold text-3xl flex items-center justify-center'>
-          물품관리서비스 <span className='text-main pl-2'>로그인</span>
+        <h1 className='w-80 h-10 font-bold text-3xl flex items-center justify-center'>
+          물품관리서비스 <span className='text-main pl-2'>회원가입</span>
         </h1>
-        <h3 className='w-210 h-4 text-xs text-gray text-center'>
-          물품관리서비스로 물품을 관리하세요 {':)'}
-        </h3>
+        <div>
+          <input
+            placeholder='Email'
+            className='w-72 h-10 pl-3 outline-none border-b-2 bg-white'
+            name='username'
+            onChange={onChangeInputs}
+          />
+          <FontAwesomeIcon icon={faEnvelope} className='relative right-6' />
+        </div>
         <div>
           <input
             placeholder='Username'
@@ -78,23 +89,36 @@ const LoginPage = () => {
             />
           )}
         </div>
+        <div>
+          <input
+            placeholder='Password Confirm'
+            type={isShowPass ? 'text' : 'password'}
+            className='w-72 h-10 pl-3 outline-none border-b-2'
+            name='passwordConfirm'
+            onChange={onChangeInputs}
+          />
+          {isShowPass ? (
+            <FontAwesomeIcon
+              icon={faEye}
+              className='relative right-6 cursor-pointer'
+              onClick={onClickShowPassConfirm}
+            />
+          ) : (
+            <FontAwesomeIcon
+              icon={faEyeSlash}
+              className='relative right-6 cursor-pointer'
+              onClick={onClickShowPassConfirm}
+            />
+          )}
+        </div>
         <button
           className='w-430 h-46 flex items-center justify-center bg-main rounded-md hover:opacity-95'
-          onClick={onClickLogin}
+          onClick={onClickRegister}
         >
-          LOGIN
+          REGISTER
         </button>
-        <div className='text-xs text-gray text-center'>
-          아직 회원이 아니신가요?
-          <span
-            className='text-main cursor-pointer hover:opacity-95 pl-1'
-            onClick={onClickMoveToRegister}
-          >
-            회원가입
-          </span>
-        </div>
       </div>
     </div>
   )
 }
-export default LoginPage
+export default Register
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,14 @@
 import { NavigationUtil } from '@/utils'
 import { Routes, Route, Navigate } from 'react-router-dom'
 import LoginPage from '@/pages/login'
+import Register from '@/pages/register'
 
 const Router = () => {
   return (
     <Routes>
       <Route path='/' element={<Navigate replace to='/login' />} />
       <Route path={NavigationUtil.login} element={<LoginPage />} />
+      <Route path={NavigationUtil.register} element={<Register />} />
     </Routes>
   )
 }
