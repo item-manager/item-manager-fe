43163a1109f7ffca6e98f3d6ce130638d88559c2
Young Jin Song <subutuo@naver.com>
2023-02-28 21:39:16 +0900

    feat: 위치정보 삭제

--- src/pages/locations/tables/RoomTable.tsx
+++ src/pages/locations/tables/RoomTable.tsx
@@ -1,17 +1,20 @@
 import { httpClient, RoomsRS } from '@/apis'
 import BasicTable from '@/components/tables/BasicTable'
 import useModal from '@/hooks/useModal'
+import { isContentLoadingState } from '@/store'
 import { DeleteFilled, EditOutlined, EllipsisOutlined } from '@ant-design/icons'
-import { useQuery } from '@tanstack/react-query'
-import { Button, Space, Tooltip } from 'antd'
+import { useQuery, useQueryClient } from '@tanstack/react-query'
+import { Button, message, Space, Tooltip } from 'antd'
 import { ColumnsType } from 'antd/es/table'
 import { useState } from 'react'
+import { useSetRecoilState } from 'recoil'
 import RoomModal from '../modals/RoomModal'
 
 const RoomTable = ({ setRoomNo }: { setRoomNo: (roomNo: number) => void }) => {
   const { visible, showModal, hideModal } = useModal()
-
   const [record, setRecord] = useState<RoomsRS | undefined>()
+  const setIsLoadingState = useSetRecoilState(isContentLoadingState)
+  const queryClient = useQueryClient()
 
   const query = useQuery({
     queryKey: ['rooms'],
@@ -28,7 +31,41 @@ const RoomTable = ({ setRoomNo }: { setRoomNo: (roomNo: number) => void }) => {
     showModal()
   }
 
-  const deleteRoom = (_record: RoomsRS) => {}
+  const deleteRoom = async (record: RoomsRS) => {
+    setIsLoadingState(true)
+    try {
+      const locationNo = record.roomNo
+
+      // 1. 사용중인 물품정보 조회
+      const result = await httpClient.items.getItemsInLocation({ locationNo })
+      if (result.data?.length) {
+        // TODO 모달이든 어떻게든 변경
+        alert(
+          `메시지 변경 예정\n사용 중인 물품 : ${result.data.map((item) => item.name).join(', ')}`
+        )
+        return
+      }
+
+      // 2. 위치데이터 있는지 확인
+      // TODO 위치정보를 방 조회시 미리 가지고 있을지 확인
+      const placesResult = await httpClient.locations.getPlacesByRoomNo({ roomNo: locationNo })
+      if (placesResult.data?.length) {
+        // TODO 모달이든 어떻게든 변경
+        alert('등록된 위치정보가 있어 삭제할 수 없습니다.')
+        return
+      }
+
+      // 3. 삭제
+      // TODO 에러 처리?
+      await httpClient.locations.deleteLocation(locationNo)
+      queryClient.invalidateQueries({ queryKey: ['rooms'], exact: true })
+      message.success('삭제되었습니다.')
+    } catch (e) {
+      console.error(e)
+    } finally {
+      setIsLoadingState(false)
+    }
+  }
 
   const columns: ColumnsType<RoomsRS> = [
     {
