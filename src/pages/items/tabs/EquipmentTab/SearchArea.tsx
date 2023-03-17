27c3b1b9678bd77458ec876f12988350f7636181
Young Jin Song <subutuo@naver.com>
2023-03-18 00:17:58 +0900

    fix: 소모품/비품 조회 - 데이터에없는 라벨로 조회 못하도록 수정

--- src/pages/items/tabs/EquipmentTab/SearchArea.tsx
+++ src/pages/items/tabs/EquipmentTab/SearchArea.tsx
@@ -106,7 +106,7 @@ const SearchArea = () => {
             <Select<string[]>
               className='min-w-[100px]'
               allowClear
-              mode='tags'
+              mode='multiple'
               placeholder='라벨 검색'
               options={getLabelsQuery.data}
               loading={getLabelsQuery.isLoading}
