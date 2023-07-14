afe34477928e1af0044f0290028d09fefd140f7c
yurim <yrim113@gmail.com>
2023-07-14 21:39:14 +0900

    Feat: 물품 상세 변경

--- src/pages/detail/index.tsx
+++ src/pages/detail/index.tsx
@@ -2,31 +2,106 @@ import { httpClient } from '@/apis'
 import { PriorityProgressBar } from '@/components/progress'
 import { historyTab } from '@/store/history'
 import { useQuery } from '@tanstack/react-query'
-import { Space, Tabs, TabsProps, Typography } from 'antd'
-import { useParams } from 'react-router'
+import { Button, Tabs, TabsProps, Typography, Modal, message } from 'antd'
+import { useParams, useNavigate } from 'react-router'
 import { useRecoilState } from 'recoil'
 import ItemDetail from './components/itemDetail'
 import ItemHistory from './components/itemHistory'
+import useModal from '@/hooks/useModal'
+import ItemEditModal from './components/itemDetail/modal/itemEditModal'
+import { NavigationUtil } from '@/utils'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrashCan, faPenToSquare } from '@fortawesome/free-regular-svg-icons'
+import { useState } from 'react'
 
 const DetailPage = () => {
   const { itemNo } = useParams()
+  const { isEdit, hideEditModal, showEditModal } = useModal()
+  const [modal, contextHolder] = Modal.useModal()
+
+  const navigate = useNavigate()
 
   const { data: itemDetail } = useQuery(['items'], () => httpClient.items.getItem(Number(itemNo)))
 
   const [isHistory, setIsHistory] = useRecoilState(historyTab)
 
-  const Detail = () => {
-    return (
-      <Space wrap size={14} className='mb-2'>
-        <Typography.Title level={2} className='flex items-center m-0'>
+  const onClickMovePrev = () => {
+    navigate(NavigationUtil.items)
+  }
+
+  const [deleteHover, setDeleteHover] = useState(false)
+  const [editHover, setEditHover] = useState(false)
+
+  const onClickDeleteItem = () => {
+    modal.confirm({
+      title: '물품 삭제',
+      content: (
+        <>
+          해당 물품(<b>{itemDetail?.data?.name}</b>)을 삭제하시겠습니까?
+        </>
+      ),
+      onOk: async () => {
+        await httpClient.items.deleteItem(Number(itemNo))
+        message.success('삭제되었습니다.')
+        navigate(NavigationUtil.items)
+      },
+    })
+  }
+
+  const Detail = () => (
+    <div className='grid grid-cols-2'>
+      <div className='block justify-self-start ml-2'>
+        <Button type='link' className='flex' onClick={onClickMovePrev}>
+          목록으로
+        </Button>
+        <Typography.Title level={2} className='flex items-center mt-2'>
           <div className='w-6 mr-2'>
             <PriorityProgressBar priority={itemDetail?.data?.priority} strokeWidth={4} />
           </div>
           {itemDetail?.data?.name}
         </Typography.Title>
-      </Space>
-    )
-  }
+      </div>
+      <div className='flex justify-self-end items-end'>
+        <div className='flex mr-8 grid grid-cols-2 gap-x-4'>
+          <div className='w-10 h-10'>
+            <FontAwesomeIcon
+              icon={faPenToSquare}
+              size='2xl'
+              className={
+                'text-emerald-900 hover:cursor-pointer hover:text-4xl' +
+                (editHover ? ' fa-beat' : '')
+              }
+              // onMouseEnter={() => {
+              //   setEditHover(true)
+              // }}
+              // onMouseLeave={() => {
+              //   setEditHover(false)
+              // }}
+              onClick={showEditModal}
+            />
+            {isEdit && <ItemEditModal hideModal={hideEditModal} itemDetail={itemDetail?.data} />}
+          </div>
+          <div className='w-10 h-10'>
+            <FontAwesomeIcon
+              icon={faTrashCan}
+              size='2xl'
+              className={
+                'text-emerald-900 hover:cursor-pointer hover:text-4xl' +
+                (deleteHover ? ' fa-shake' : '')
+              }
+              onMouseEnter={() => {
+                setDeleteHover(true)
+              }}
+              onMouseLeave={() => {
+                setDeleteHover(false)
+              }}
+              onClick={onClickDeleteItem}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 
   const items: TabsProps['items'] = [
     {
@@ -54,6 +129,7 @@ const DetailPage = () => {
         items={items}
         onChange={onChangeIsHistory}
       />
+      {contextHolder}
     </>
   )
 }
